2af3b042c75375f81f9a3a23771d7d2a1279364c
dener120 <dener7a23c@gmail.com>
2022-06-29 19:59:04 +0300

    commit changes nav

--- font-end/src/App.js
+++ font-end/src/App.js
@@ -6,7 +6,7 @@ import OrderList from "./components/pages/OrderList";
 import {useStore} from "./components/providers/RootStoreProvider";
 import {useEffect} from "react";
 import Modal from "./components/Modal";
-import AuthReg from "./components/Forms/Auth-Reg/AuthReg";
+import AuthReg from "./components/Forms/AuthReg";
 
 
 function App() {
